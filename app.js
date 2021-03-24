b4dd36c3790c58d3776509891cc3610a891704d3
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2021-03-24 12:21:04 -0500

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -13,9 +14,9 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
   var data = {
     members: [
@@ -30,7 +31,14 @@ app.post("/", function(req, res){
     ]
   };
 
-  var jsonData = JSON.stringify(data);
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us1.api.mailchimp.com/3.0/lists/6b2cb14e02";
+
+  https.request(url, options, function(response){
+
+
+  });
 
 });
 

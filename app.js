d40dfaf91413a8646ec28388642295c007d30584
cuvillier.ryan@gmail.com <cuvillier.ryan@gmail.com>
2021-03-27 09:31:11 -0500

    canceleApi

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ app.post("/", function(req, res) {
 
   const jsonData = JSON.stringify(data);
 
-  const url = "https://us1.api.mailchimp.com/3.0/lists/6b2cb14e02";
+  const url = 
 
   const options = {
     method: "POST",
-    auth: "ryan1:424205e97acb09f04facacf6cd8b0d21-us1"
+    auth:
   }
 
   const request = https.request(url, options, function(response) {
